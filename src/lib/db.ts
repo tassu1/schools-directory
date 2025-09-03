e5e880323e0c7a24a3fe019bd8265a2aaddd7a22
Tahseen <tassutahsee@gmail.com>
2025-09-03 21:38:27 +0530

    created db connection

---
+++ src/lib/db.ts
@@ -0,0 +1,12 @@
+import mysql from "mysql2/promise";
+
+export async function connectDB() {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+  return connection;
+}
