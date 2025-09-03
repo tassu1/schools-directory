5718af039bd406f836de61a6eb984fc80c9a14d7
Tahseen <tassutahsee@gmail.com>
2025-09-04 00:54:24 +0530

    change db connection

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,20 @@
 import mysql from "mysql2/promise";
 
 export async function connectDB() {
+  const isProduction = process.env.NODE_ENV === "production";
+
   const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: isProduction
+      ? {
+          rejectUnauthorized: false, 
+        }
+      : undefined,
   });
+
   return connection;
 }
